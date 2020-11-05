cf223996ac6c2e4d1f94943aa020983e4aa2603f
fragment0 <hi@fragment0.com>
2020-11-05 11:46:19 +0800

    remote tern; update custom

--- custom.el
+++ custom.el
@@ -1,2 +1,4 @@
-;; (set-frame-font "DejaVu Sans Mono-13")
-;; (set-fontset-font "fontset-default" 'han "WenQuanYi Micro Hei")
+;; (set-frame-font "Consolas-13:weight=ultra-light")
+;; (set-fontset-font "fontset-default" 'han "Microsoft Yahei-11")
+;; (setq face-font-rescale-alist '(("Microsoft Yahei" . 1.1)))
+;; (cd "c:/Users/xinzh")
